e1481b024bdf4619b3868f3579de008cf53bf42e
yarik-gulya <profesoryarik2@gmail.com>
2025-06-04 20:28:27 +0300

    add site-header.vue

---
+++ my-vue-app/src/components/site-header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <h1>Мій перший Vue-додаток! </h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'SiteHeader'
+}
+</script>
+
+<style scoped>
+header {
+  background-color: #42b983;
+  padding: 15px;
+  color: white;
+  text-align: center;
+}
+</style>
